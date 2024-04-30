2fd5813163fce3a43cef1779a458c81cb8511e55
isabela da silva biacca <isa.biacca08@gmail.com>
2024-04-30 10:40:16 -0300

    app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const tarefaRouter = require("./routes/tarefaRouter");
 
 // Configuração do diretório de arquivos estáticos
 app.use(express.static("public"));
+
 // Configuração das rotas e do parsing do corpo da requisição
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use("/", tarefaRouter);
 // Inicialização do servidor
